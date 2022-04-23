0d3ad8dde7f8c7ff2cee4ef60abae47ead7b1dda
pd-allen <43551964+pd-allen@users.noreply.github.com>
2022-04-23 16:35:21 -0400

    change file names

--- apps/vector_data.py
+++ apps/vector_data.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-
+import os.path
+from os import path
 
 def app():
 
@@ -10,13 +11,19 @@ def app():
     dem = os.path.join(out_dir, 'dem.tif')
     
     landsat = os.path.join(out_dir, 'landsat.tif')
+    
     if not os.path.exists(dem):
         dem_url = 'https://drive.google.com/file/d/1vRkAWQYsLWCi6vcTMk8vLxoXMFbdMFn8/view?usp=sharing'
         leafmap.download_from_gdrive(dem_url, dem, out_dir, unzip=False)
     if not os.path.exists(landsat):
         landsat_url = 'https://github.com/giswqs/leafmap/raw/master/examples/data/cog.tif'
         leafmap.download_from_url(landsat_url, landsat, out_dir, unzip=False)   
+    
     m = leafmap.Map()
-    m.add_local_tile(landsat, band=[4, 3, 2], layer_name="Landsat")
+    
     m.add_local_tile(dem, palette='viridis', layer_name="DEM")
-    m.to_streamlit(height=700)
+    m.add_local_tile(landsat,  layer_name="Landsat")
+    m.to_streamlit(height=900,width=1200)
+    
+app()
+       
